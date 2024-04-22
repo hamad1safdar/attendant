cebc0a7bab267ddc0f1be186c59e2ad5f50a87e7
Hamad Safdar <hamad.safdar@emumba.com>
2024-04-22 07:59:28 +0500

    Added env

--- src/services/gists.ts
+++ src/services/gists.ts
@@ -1,16 +1,12 @@
 import axios from 'axios';
 import type { Gist, GistFile, User } from '../types';
 
-import { GIT_ACCESS_TOKEN } from '../secrets';
-
 const GIST_ENDPOINT = 'https://api.github.com/gists';
 
 const USER_GIST_ID = 'ed8789dac7cc871da7e3b421902bcd79';
 const USER_GIST_NAME = 'users.json';
-// const RECORDS_GIST_ID = '447fd59bfed3518a058eef08d9dae3a3';
-// const RECORDS_GIST_NAME = 'records.json';
-// const HELPER_DATA_GIST_ID = 'c099f4a808aab569a5262a4ce6bdb1c4';
-// const HELPER_DATA_GIST_NAME = 'helper_data.json';
+
+const GIT_ACCESS_TOKEN = import.meta.env.VITE_GIT_API_TOKEN;
 
 export const getUsers = async (): Promise<Array<User>> => {
     const response = await getGistById(USER_GIST_ID);
--- src/utils.ts
+++ src/utils.ts
@@ -8,8 +8,6 @@ export const getToday = () => dayjs().format(DATE_FORMAT);
 export const getCurrentTime = () => dayjs().format('HH:mm');
 export const getFormattedDate = (date: Date) => dayjs(date).format(DATE_FORMAT);
 export const unformatDate = (formattedDate: string) => {
-    console.log(formattedDate);
     const r = dayjs(formattedDate, DATE_FORMAT).format('YYYY-MM-DD');
-    console.log(r);
     return r;
 };
